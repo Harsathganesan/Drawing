a35f7ef41a6134392e3a721c88137bb7282d7a67
Harsathganesan <harsath137@gmail.com>
2026-05-15 10:27:12 +0530

    mn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,36 @@ import Gallery from './components/Gallery';
 import Contact from './components/Contact';
 import Order from './components/order';
 import Footer from './components/Footer';
+import Lenis from 'lenis';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <div className="App">
